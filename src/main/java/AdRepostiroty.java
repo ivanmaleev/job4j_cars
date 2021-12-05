1bc6c3390c3d7a808523666de6af3c4f81c73902
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-12-05 16:08:54 +0300

    Реализовать пользовательские фильтры для площадок машин [#4745]

--- src/main/java/AdRepostiroty.java
+++ src/main/java/AdRepostiroty.java
@@ -1,2 +1,59 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
+import ru.job4j.model.Advertisement;
+import ru.job4j.model.CarBrand;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class AdRepostiroty {
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure().build();
+    private final SessionFactory sf = new MetadataSources(registry)
+            .buildMetadata().buildSessionFactory();
+
+    public List<Advertisement> findAdsLastDay() {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        Query query = session.createQuery(
+                "from Advertisement ad "
+                        + "where ad.created between :startdate and :enddate");
+        query.setParameter("startdate", LocalDate.now().minusDays(1));
+        query.setParameter("enddate", LocalDate.now());
+        List<Advertisement> ads = query.list();
+        session.getTransaction().commit();
+        session.close();
+        return ads;
+    }
+
+    public List<Advertisement> findAdsWithPhoto() {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        Query query = session.createQuery(
+                "from Advertisement ad "
+                        + "join fetch ad.car car "
+                        + "where car.photoPath != '' and car.photoPath != null");
+        List<Advertisement> ads = query.list();
+        session.getTransaction().commit();
+        session.close();
+        return ads;
+    }
+
+    public List<Advertisement> findAdsByBrand(CarBrand brand) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        Query query = session.createQuery(
+                "from Advertisement ad "
+                        + "join fetch ad.car car "
+                        + "where car.brand = :brand");
+        query.setParameter("brand", brand);
+        List<Advertisement> ads = query.list();
+        session.getTransaction().commit();
+        session.close();
+        return ads;
+    }
 }
--- src/main/java/ru/job4j/model/Advertisement.java
+++ src/main/java/ru/job4j/model/Advertisement.java
@@ -1,6 +1,7 @@
 package ru.job4j.model;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "advertisements")
@@ -12,4 +13,45 @@ public class Advertisement {
     @OneToOne
     private Car car;
     private boolean sold;
+    private Date created;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public boolean isSold() {
+        return sold;
+    }
+
+    public void setSold(boolean sold) {
+        this.sold = sold;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
 }
