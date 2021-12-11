a70d50d7d02d3947165293317df1c9a82d3db496
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-12-11 09:01:40 +0300

    Реализовать пользовательские фильтры для площадок машин [#4745 #236190]

--- src/main/java/AdRepostiroty.java
+++ src/main/java/AdRepostiroty.java
@@ -11,51 +11,18 @@ import org.hibernate.query.Query;
 import ru.job4j.model.Advertisement;
 import ru.job4j.model.CarBrand;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Properties;
 import java.util.function.Function;
 
 public class AdRepostiroty {
 
     private static final AdRepostiroty INSTANCE = new AdRepostiroty();
 
-    private static final Logger LOG = LogManager.getLogger(AdRepostiroty.class.getName());
-
-    private final BasicDataSource pool = new BasicDataSource();
-
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
             .configure().build();
     private final SessionFactory sf = new MetadataSources(registry)
             .buildMetadata().buildSessionFactory();
-
-    private AdRepostiroty() {
-        Properties cfg = new Properties();
-        try (BufferedReader io = new BufferedReader(
-                new InputStreamReader(
-                        AdRepostiroty.class.getClassLoader()
-                                .getResourceAsStream("db.properties")
-                )
-        )) {
-            cfg.load(io);
-        } catch (Exception e) {
-            LOG.error("Invalid parameters", e);
-        }
-        try {
-            Class.forName(cfg.getProperty("jdbc.driver"));
-        } catch (Exception e) {
-            LOG.error("Driver not loaded", e);
-        }
-        pool.setDriverClassName(cfg.getProperty("jdbc.driver"));
-        pool.setUrl(cfg.getProperty("jdbc.url"));
-        pool.setUsername(cfg.getProperty("jdbc.username"));
-        pool.setPassword(cfg.getProperty("jdbc.password"));
-        pool.setMinIdle(5);
-        pool.setMaxIdle(10);
-        pool.setMaxOpenPreparedStatements(100);
-    }
 
     private static final class Lazy {
         private static final AdRepostiroty INST = new AdRepostiroty();
